8e9f29b9c6ad81824fa5d2ca8aa37b41b0941a49
David Gilardi <david.gilardi@datastax.com>
2018-06-06 11:00:50 -0400

    Properly removing the extras directory

--- extras/_with_server_and_external_up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-COMMON_DIR="lib/killrvideo-docker-common"
-EXTRAS_DIR="$COMMON_DIR/extras"
-
-docker-compose -f $COMMON_DIR/docker-compose.yaml -f $EXTRAS_DIR/docker-compose-dse-external.yaml -f docker-compose.yaml -f $EXTRAS_DIR/docker-compose-server.yaml up -d
