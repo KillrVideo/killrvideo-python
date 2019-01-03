4e5746425fdffc631cac8e6b211be813fbbf7f31
amandamoran <amandakaymoran@gmail.com>
2019-01-02 17:15:25 -0800

    Still getting timeUUID error

--- killrvideo/comments/comments_service_grpc.py
+++ killrvideo/comments/comments_service_grpc.py
@@ -1,14 +1,9 @@
 import grpc
-<<<<<<< HEAD
+import logging
 from uuid import UUID
 from time_uuid import TimeUUID
 from common.common_types_conversions import UUID_to_grpc, grpc_to_UUID, grpc_totimeUUID
 from comments_service_pb2 import CommentOnVideoRequest, CommentOnVideoResponse, GetUserCommentsRequest, GetUserCommentsResponse, GetVideoCommentsRequest, GetVideoCommentsResponse
-=======
-import logging
-from common.common_types_conversions import UUID_to_grpc, grpc_to_UUID
-from comments_service_pb2 import CommentOnVideoResponse
->>>>>>> 7512ee207effceb1ee60b329ed4c604b3820e528
 import comments_service_pb2_grpc
 
 def CommentsByUserModel_to_GetUserComments(result):
@@ -17,9 +12,9 @@ def CommentsByUserModel_to_GetUserComments(result):
 def UserComments_to_GetUserCommentsResponse(results):
     response = GetUserCommentsResponse(paging_state=results.paging_state)
     if isinstance(results, (list,)):    # most preferred way to check if it's list
-        response.comments.extend(map(CommentsByUserModel_to_GetUserComments, results.comments))
+        response.comments.extend(map(CommentsByUserModel_to_GetUserComments, results))
     elif results is not None:  # single result
-        response.comments.extend([CommentsByUserModel_to_GetUserComments(results.comments)])
+        response.comments.extend([CommentsByUserModel_to_GetUserComments(results)])
     return response
 
 class CommentsServiceServicer(comments_service_pb2_grpc.CommentsServiceServicer):
@@ -35,8 +30,10 @@ class CommentsServiceServicer(comments_service_pb2_grpc.CommentsServiceServicer)
         """
         print ">>> CommentsService:CommentOnVideo: "
         print request
-        self.comments_service.comment_on_video(grpc_to_UUID(request.video_id.value),
-                                               grpc_to_UUID(request.user_id.value), request.comment)
+        self.comments_service.comment_on_video(UUID(request.video_id.value),
+                                               UUID(request.user_id.value), 
+                                               UUID (request.comment_id.value), 
+                                               request.comment)
         return CommentOnVideoResponse()
 
     def GetUserComments(self, request, context):
@@ -48,8 +45,8 @@ class CommentsServiceServicer(comments_service_pb2_grpc.CommentsServiceServicer)
         if request.starting_comment_id.value:
             starting_comment_id = grpc_to_UUID(request.starting_comment_id)
         print "here"
-        result = self.comments_service.get_user_comments(user_id=grpc_to_UUID(request.user_id.value), page_size=request.page_size, starting_comment_id=grpc_toUUID(request.starting_comment_id.value), paging_state=request.paging_state)
-        print result
+        result = self.comments_service.get_user_comments(user_id=grpc_to_UUID(request.user_id), page_size=request.page_size, starting_comment_id=starting_comment_id, paging_state=request.paging_state)
+        print "there"
         return UserComments_to_GetUserCommentsResponse(result)
 
     def GetVideoComments(self, request, context):
