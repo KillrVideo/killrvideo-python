ef98385c6c5f052d716ab9f168ca384daef8cf39
Jeffrey Carpenter <jeffreyscarpenter@cox.net>
2019-06-21 17:11:48 -0700

    fixing Travis build scripts to be bash

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script assumes the following environment variables are present
 #
--- scripts/travis-publish.sh
+++ scripts/travis-publish.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e # Exit with nonzero exit code if anything fails
 
