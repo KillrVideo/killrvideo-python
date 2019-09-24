bd12a38f0e934b2fe8a95dbc42448f5aa8c6d5cf
Eric Zietlow <evemsmando@gmail.com>
2019-09-24 09:09:47 -0400

    Update video_catalog_events_kafka.py
    commented out unused python code

--- killrvideo/video_catalog/video_catalog_events_kafka.py
+++ killrvideo/video_catalog/video_catalog_events_kafka.py
@@ -10,16 +10,17 @@ class VideoCatalogPublisher(object):
     """Provides methods that publish events associated with the Video Catalog Service."""
 
     def __init__(self):
-        self.producer = KafkaProducer(bootstrap_servers=os.getenv('KILLRVIDEO_KAFKA_BOOTSTRAP_SERVERS', 'kafka'),
-                                      client_id='killrvideo-python:VideoCatalogService')
-
+#        self.producer = KafkaProducer(bootstrap_servers=os.getenv('KILLRVIDEO_KAFKA_BOOTSTRAP_SERVERS', 'kafka'),
+#                                      client_id='killrvideo-python:VideoCatalogService')
+        pass
 
     def publish_youtube_video_added_event(self, video_id, user_id, name, description, tags, location,
                                           preview_image_location, added_date, timestamp):
-        event = YouTubeVideoAdded(video_id=UUID_to_grpc(video_id), user_id=UUID_to_grpc(user_id), name=name,
-                                  description=description, tags=tags, location=location,
-                                  preview_image_location=preview_image_location,
-                                  added_date=datetime_to_Timestamp(added_date),
-                                  timestamp=datetime_to_Timestamp(timestamp))
-
-        self.producer.send(YOUTUBE_VIDEO_ADDED_TOPIC, event.SerializeToString())
+#        event = YouTubeVideoAdded(video_id=UUID_to_grpc(video_id), user_id=UUID_to_grpc(user_id), name=name,
+#                                  description=description, tags=tags, location=location,
+#                                  preview_image_location=preview_image_location,
+#                                  added_date=datetime_to_Timestamp(added_date),
+#                                  timestamp=datetime_to_Timestamp(timestamp))
+#
+#        self.producer.send(YOUTUBE_VIDEO_ADDED_TOPIC, event.SerializeToString())
+        pass
